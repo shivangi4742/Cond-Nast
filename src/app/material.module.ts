168f14cbe26922796eb05eb70398bbf054e4a493
shivangi <shivangi4742>
2021-03-29 22:41:40 +0530

    second commit

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
@@ -19,11 +15,7 @@ const MaterialComponents = [
   MatCardModule,
   MatSidenavModule,
   MatMenuModule,
-  MatGridListModule,
-  MatPaginatorModule,
-  MatTableModule,
-  MatDividerModule,
-  MatProgressSpinnerModule,
+  MatGridListModule
 ];
 
 @NgModule({
--- src/app/services/news-api.service.ts
+++ src/app/services/news-api.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { environment } from "../../environments/environment";
-
+import { throwError } from "rxjs";
+import { catchError } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
-  api_key: string = environment.news_api.api_key;
+  apiKey: string = environment.news_api.api_key;
 
   constructor(private http: HttpClient) {}
+
   getSourceFromLanguage() {
-    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.apiKey)
+                    .pipe(catchError(this.handleError));
  }
 
  getTechCrunch() {
-  return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.apiKey)
+                  .pipe(catchError(this.handleError));
  }
 
  getTopheadlinesFromSource(source: String) {
-  return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.apiKey)
+                  .pipe(catchError(this.handleError));
  }
+
+ handleError(error:HttpErrorResponse) {
+  let errorMessage = '';
+  if (error.error instanceof ErrorEvent) {
+    // client-side error
+    errorMessage = `Error: ${error.error.message}`;
+  } else {
+    // server-side error
+    errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+  }
+  window.alert(errorMessage);
+  return throwError(errorMessage);
+}
 }
