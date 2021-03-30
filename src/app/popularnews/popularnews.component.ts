4f05de0b58ef11a58a257fe1d053eb127624c8cb
shivangi <shivangi4742>
2021-03-30 13:59:15 +0530

    first commit

--- src/app/popularnews/popularnews.component.ts
+++ src/app/popularnews/popularnews.component.ts
@@ -7,18 +7,18 @@ import { NewsApiService } from '../services/news-api.service';
   styleUrls: ['./popularnews.component.css']
 })
 export class PopularnewsComponent implements OnInit {
-  mArticles!: Array<any>;
-  mSources!: Array<any>;
+  articlesList!: Array<any>;
+  sourcesList!: Array<any>;
 
   constructor(private newsapi: NewsApiService) {}
   
   ngOnInit() {
-    this.newsapi.getTechCrunch().subscribe((data:any) => this.mArticles = data['articles']);
-    this.newsapi.getSourceFromLanguage().subscribe((data:any) => this.mSources = data['sources']);
+    this.newsapi.getTechCrunch().subscribe((data:any) => this.articlesList = data['articles']);
+    this.newsapi.getSourceFromLanguage().subscribe((data:any) => this.sourcesList = data['sources']);
   }
 
   searchArticles(source: any) {
-    this.newsapi.getTopheadlinesFromSource(source).subscribe((data: any) => this.mArticles = data['articles']);
+    this.newsapi.getTopheadlinesFromSource(source).subscribe((data: any) => this.articlesList = data['articles']);
   }
 
 }
